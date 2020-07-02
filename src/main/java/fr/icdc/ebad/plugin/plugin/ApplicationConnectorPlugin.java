eb0181362211b8eb4d7e67a159760cb263a13f40
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2020-07-02 15:36:43 +0200

    Upgrade pf4j

--- src/main/java/fr/icdc/ebad/plugin/plugin/ApplicationConnectorPlugin.java
+++ src/main/java/fr/icdc/ebad/plugin/plugin/ApplicationConnectorPlugin.java
@@ -1,13 +1,11 @@
 package fr.icdc.ebad.plugin.plugin;
 
 import fr.icdc.ebad.plugin.dto.ApplicationDiscoverDto;
-import fr.icdc.ebad.plugin.dto.EnvironnementDiscoverDto;
-import fr.icdc.ebad.plugin.dto.NormeDiscoverDto;
 import org.pf4j.ExtensionPoint;
-import org.pf4j.PluginException;
+import org.pf4j.PluginRuntimeException;
 
 import java.util.List;
 
 public interface ApplicationConnectorPlugin extends ExtensionPoint {
-    public List<ApplicationDiscoverDto> discoverApp() throws PluginException;
+    public List<ApplicationDiscoverDto> discoverApp();
 }
--- src/main/java/fr/icdc/ebad/plugin/plugin/EnvironnementConnectorPlugin.java
+++ src/main/java/fr/icdc/ebad/plugin/plugin/EnvironnementConnectorPlugin.java
@@ -3,10 +3,10 @@ package fr.icdc.ebad.plugin.plugin;
 import fr.icdc.ebad.plugin.dto.EnvironnementDiscoverDto;
 import fr.icdc.ebad.plugin.dto.NormeDiscoverDto;
 import org.pf4j.ExtensionPoint;
-import org.pf4j.PluginException;
+import org.pf4j.PluginRuntimeException;
 
 import java.util.List;
 
 public interface EnvironnementConnectorPlugin extends ExtensionPoint {
-    public List<EnvironnementDiscoverDto> discoverFromApp(String app, String appName, List<NormeDiscoverDto> norms) throws PluginException;
+    public List<EnvironnementDiscoverDto> discoverFromApp(String app, String appName, List<NormeDiscoverDto> norms);
 }
